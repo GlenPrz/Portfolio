4377877824091c70d2b0354b129dc9007bd0b6ff
GlenPrz <glenwilliam.perez36@gmail.com>
2025-05-29 08:20:58 +0800

    modified:   src/components/header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,7 @@
-import React from 'react';
-import { MDBBtn } from 'mdb-react-ui-kit';
-import TypewriterEffect from './TypewriterEffect'; // Import the TypewriterEffect component
-import '../styles/header.css'; // Assuming you have a CSS file for styles
-import { FaFacebook, FaLinkedin, FaGithub } from 'react-icons/fa';
-
+import { MDBBtn } from "mdb-react-ui-kit";
+import TypewriterEffect from "./TypewriterEffect"; // Import the TypewriterEffect component
+import "../styles/header.css"; // Assuming you have a CSS file for styles
+import { FaFacebook, FaLinkedin, FaGithub } from "react-icons/fa";
 
 const HeroSection = () => {
   return (
@@ -11,26 +9,38 @@ const HeroSection = () => {
       <div className="hero-content-left">
         {/* Integrating the TypewriterEffect component here */}
         <h1 className="hero-title">Hi, I'm Glen</h1>
-        <TypewriterEffect textArray={[ "I'm a Developer", "I'm a Freelancer", "Let's bring ideas to life together!", "Contact me and let's connect!", ]} />
-        <button type="button" className="btn btn-primary">Contact Me!</button>
-        <div className="icon-container" style={{ display: 'flex', gap: '16px', marginTop: '20px' }}>
+        <TypewriterEffect
+          textArray={[
+            "I'm a Developer",
+            "I'm a Freelancer",
+            "Let's bring ideas to life together!",
+            "Contact me and let's connect!",
+          ]}
+        />
+        <button type="button" className="btn btn-primary">
+          Contact Me!
+        </button>
+        <div
+          className="icon-container"
+          style={{ display: "flex", gap: "16px", marginTop: "20px" }}
+        >
           {/* Facebook */}
           <a
             href="https://www.facebook.com/share/1AucbBFk8C/?mibextid=wwXIfr"
             target="_blank"
             rel="noopener noreferrer"
-            className='icon-container ic1'
+            className="icon-container ic1"
           >
-            <FaFacebook className='icons' size={25} color="#1877F2" />
+            <FaFacebook className="icons" size={25} color="#1877F2" />
           </a>
           {/* Linkedin */}
           <a
             href="http://www.linkedin.com/in/glen-william-perez-098801307"
             target="_blank"
             rel="noopener noreferrer"
-            className='icon-container ic2'
+            className="icon-container ic2"
           >
-            <FaLinkedin className='icons' size={25} color="#0A66C2" />
+            <FaLinkedin className="icons" size={25} color="#0A66C2" />
           </a>
 
           {/* Github */}
@@ -38,9 +48,9 @@ const HeroSection = () => {
             href="https://github.com/GlenPrz"
             target="_blank"
             rel="noopener noreferrer"
-            className='icon-container ic3'
+            className="icon-container ic3"
           >
-            <FaGithub className='icons' size={25} color="#333" />
+            <FaGithub className="icons" size={25} color="#333" />
           </a>
         </div>
       </div>
